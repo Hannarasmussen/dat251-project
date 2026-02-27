acb00616fc0230ebd6d67abbbf406674b4533a31
SigvardHelsingen <sihel6491@hvl.no>
2026-02-27 12:10:51 +0100

    postgres database support, user & recipe models/controller/service/repo

---
+++ backend/src/main/java/com/example/dat251_greengafl/controller/RecipeController.java
@@ -0,0 +1,47 @@
+package com.example.dat251_greengafl.controller;
+
+import com.example.dat251_greengafl.model.Recipe;
+import com.example.dat251_greengafl.service.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/recipe")
+public class RecipeController {
+
+    @Autowired
+    private RecipeService recipeService;
+
+    @GetMapping
+    public List<Recipe> findAll(){
+        return recipeService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Recipe> findById(@PathVariable UUID id){
+        return recipeService.findById(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    private Recipe create(@RequestBody Recipe recipe){
+        return recipeService.register(recipe);
+    }
+
+    @PutMapping
+    public Recipe update(@RequestBody Recipe recipe){
+        return recipeService.register(recipe);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable UUID id) {
+        recipeService.deleteById(id);
+    }
+
+}
---
+++ backend/src/main/java/com/example/dat251_greengafl/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.example.dat251_greengafl.controller;
+
+import com.example.dat251_greengafl.model.User;
+import com.example.dat251_greengafl.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> findAll(){
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<User> findById(@PathVariable UUID id){
+        return userService.findById(id);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    private User create(@RequestBody User user){
+        return userService.register(user);
+    }
+
+    @PutMapping
+    public User update(@RequestBody User user){
+        return userService.register(user);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable UUID id) {
+        userService.deleteById(id);
+    }
+
+}
